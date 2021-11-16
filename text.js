4a7742be842b1960ada5da908d5900d6a8ad4f52
Michael M <83502207+madumicheal@users.noreply.github.com>
2021-11-17 00:36:28 +0545

    Add files via upload

---
+++ text.js
@@ -0,0 +1,74 @@
+alert("Hello world!");
+
+// the second alert
+function myfunction(){}
+
+//make the image invisible
+//javascript is case sensitive
+// all statements ends with a semicolon:
+//it is not sensitive to whitespace or line breaks
+//JavaScript runs from top to bottom
+
+undefined
+myVariableMicheal = 10;
+10
+var myvar2 = "hello";
+undefined
+myvar2
+'hello'
+myVar2 = 'hello again';
+'hello again'
+myVar2
+'hello again'
+var CustomerAge = 32
+undefined
+CustomerAge
+32
+var myVar = 5
+undefined
+myVar
+5
+5 + 5
+10
+myVar = myVar + 10
+15
+myVar - 3
+12
+myVar / 2
+7.5
+myVar = 7.5
+7.5
+myVar * 3
+22.5
+5 + hello
+VM1245:1 Uncaught ReferenceError: hello is not defined
+    at <anonymous>:1:5
+(anonymous) @ VM1245:1
+5 + "hello"
+'5hello'
+"madu" + " micheal"
+'madu micheal'
+5 + "hello"
+'5hello'
+5 *"hello"
+NaN
+myvar = 5
+5
+myVar **5
+23730.46875
+myVar += 5
+12.5
+myVar = 10
+10
+myVar += 5
+15
+myVar *= 5
+75
+myVar
+75
+myVar += 1
+76
+myVar++
+76
+++myVar
+78
